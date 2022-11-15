a2e86fdc94075229547aeb754bfcec9ad67e7524
Liubov.V.B <griark0611@gmail.com>
2022-11-15 11:31:17 +0300

    add Ex002

---
+++ Ex002/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("enter the first number");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("enter the second number");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2)
+{
+    Console.Write("max = ");
+    Console.WriteLine(num1);
+}
+else
+{
+    Console.Write("max = ");
+    Console.WriteLine(num2);
+}
